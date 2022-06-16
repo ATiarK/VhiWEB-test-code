ac2c8cddd792dd12a89ea00e8f45d138a3544581
Ahmad Tiar K <97088690+ATiarK@users.noreply.github.com>
2022-06-16 13:29:26 +0700

    update 404

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import ListUser from "./pages/ListUser";
 import DetailUser from "./pages/DetailUser";
+import Page404 from "./pages/Page404";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,6 +15,7 @@ root.render(
         <Route path="/" element={<Login />} />
         <Route path="/users" element={<ListUser />} />
         <Route path="/users/:id" element={<DetailUser />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
